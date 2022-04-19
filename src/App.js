89b44c47ff2bcac7267d6560010808b445748032
Ezequiel Galardi <eze3769@gmail.com>
2022-04-19 19:32:02 -0300

    solved react import issues and formatted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
-import AppContext from './context/AppContext';
-import Router from './HOC/router/Router.jsx';
-
+import "./App.css";
+import AppContext from "./context/AppContext";
+import Router from "./HOC/router/Router.jsx";
 
 function App() {
   return (
-      <AppContext>
-        <Router/>
-      </AppContext>
-  )
+    <AppContext>
+      <Router />
+    </AppContext>
+  );
 }
 
 export default App;
--- src/components/firstTimeLog/FirstTimeLog.jsx
+++ src/components/firstTimeLog/FirstTimeLog.jsx
@@ -1,37 +1,31 @@
-import React from 'react'
-import Swal from 'sweetalert2'
+import React from "react";
+import Swal from "sweetalert2";
 
 const FirstTimeLog = () => {
+  Swal.fire({
+    imageUrl: "https://i.ibb.co/vd4WxNv/confirmation-Label.jpg",
+    title: "Don't miss the trends check \n your email",
+    text: "Confirm your user account",
+    confirmButtonText: "<b>Continue<b>",
+    confirmButtonColor: "#EF62A3",
+    showCloseButton: true,
+    showClass: {
+      popup: "animate__animated animate__fadeInDown",
+    },
+    hideClass: {
+      popup: "animate__animated animate__fadeOutUp",
+    },
+    imageAlt: "errorLogin",
+    customClass: {
+      container: "sweet_container",
 
-    Swal.fire({
-                    
-        imageUrl: 'https://i.ibb.co/vd4WxNv/confirmation-Label.jpg',
-        title:"Don't miss the trends check \n your email",
-        text:"Confirm your user account",
-        confirmButtonText:'<b>Continue<b>',
-        confirmButtonColor:"#EF62A3",
-        showCloseButton: true,
-        showClass:
-        {
-            popup: 'animate__animated animate__fadeInDown'
-        },
-        hideClass:
-        {
-            popup: 'animate__animated animate__fadeOutUp'
-        },
-        imageAlt: 'errorLogin',
-        customClass: {
-            container:"sweet_container",
-            
-            title: "sweet_title",
-            text:"sweet_text",
-            confirmButton: 'button',
-            image:"sweet_image"
-        }
-      })
-  return (
-    <div>FirstTimeLog</div>
-  )
-}
+      title: "sweet_title",
+      text: "sweet_text",
+      confirmButton: "button",
+      image: "sweet_image",
+    },
+  });
+  return <div>FirstTimeLog</div>;
+};
 
-export default FirstTimeLog
+export default FirstTimeLog;
